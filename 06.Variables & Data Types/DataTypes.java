a69c694e68df856d5ceed0f3164a0031e6af4dc5
Pahari47 <nachiketapahari00@gmail.com>
2025-06-23 23:46:19 +0530

    Add JavaBasics, DataTypes, and Variables classes with example code

---
+++ 06.Variables & Data Types/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 10; // highest 256 byte can store
+        System.out.println(b);
+        char ch = 'A';
+        System.out.println(ch);
+        boolean var = true; // true or false
+        System.out.println(var);
+        float f = 10.5; //double used for large decimal numbers
+        System.out.println(f);
+        int number = 1000; // 4 bytes // long used for large numbers
+        System.out.println(number);
+        short n = 100;
+    }
+}
--- 06.Variables & Data Types/JavaBasics.java
+++ 06.Variables & Data Types/JavaBasics.java
@@ -1,5 +1,8 @@
 public class JavaBasics {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
