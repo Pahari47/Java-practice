1c4249542734e670509aceb296c32f6fdc9494c5
Pahari47 <nachiketapahari00@gmail.com>
2025-06-26 23:34:03 +0530

    Add AssignmentOperator, LogicalOperator, and RelationalOperator classes with example implementations

---
+++ 07.operators/AssignmentOperator.java
@@ -0,0 +1,11 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        // a = a + 10;
+        a += 10; // This is the assignment operator
+        int b = 20;
+        b -= 5; // Subtracting 5 from b
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ 07.operators/LogicalOperator.java
@@ -0,0 +1,7 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        System.out.println((3>2) && (5>6));
+        System.out.println((3>2) || (5>6));
+        System.out.println(!(3>2));
+    }
+}
