a06a9616bc99994ab23d8c67c1b4621be5dc7bf3
Pahari47 <nachiketapahari00@gmail.com>
2025-07-03 22:33:55 +0530

    Add Java classes for loop demonstrations including ForLoop, Print1to10, Print1ton, PrintReverseNumber, PrintSquarePattern, PrintSumofNnaturalNumbers, and WhileLoop

---
+++ 09. Loops/PrintReverseNumber.java
@@ -0,0 +1,7 @@
+public class PrintReverseNumber {
+    public static void main(String[] args) {
+        int num = 54673;
+
+        for(int i = )
+    }
+}
---
+++ 09. Loops/WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int counter = 10;
+
+        while (counter < 100) {
+            System.out.println("JAVA");
+            counter++;
+        }
+        System.out.println("END");
+    }
+}
