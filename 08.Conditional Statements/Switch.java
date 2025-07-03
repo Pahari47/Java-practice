9ee8ac89f396864caa9cca77642857783c56384f
Pahari47 <nachiketapahari00@gmail.com>
2025-07-03 20:49:12 +0530

    Add Java classes for calculator operations, leap year check, pass/fail evaluation, largest of three numbers, positive/negative check, fever detection, day of the week identification, switch case demonstration, and ternary operator usage

---
+++ 08.Conditional Statements/Switch.java
@@ -0,0 +1,12 @@
+public class Switch {
+    public static void main(String[] args) {
+        int mum = 2;
+        switch (mum) {
+            case 1: System.out.println("hii");
+                    break;
+            case 2: System.out.println("hello");
+                    break;
+            case 3: System.out.println("chal be");
+        }
+    }
+}
