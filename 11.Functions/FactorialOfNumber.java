21796de764c01b23c45bf21829ffa5dd523d1aa3
Pahari47 <nachiketapahari00@gmail.com>
2025-07-10 21:47:39 +0530

    Add FactorialOfNumber class with methods for sum calculations and prime number checks; update main method to demonstrate prime numbers in range

--- 11.Functions/FactorialOfNumber.java
+++ 11.Functions/FactorialOfNumber.java
@@ -15,7 +15,37 @@ public class FactorialOfNumber {
         int binoCoeff = fact_n / (fact_r * fact_nmr);
         return binoCoeff;
     }
+    // sum of 2 nums function
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    // sum of 3 nums function
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+    // calcuate float sum
+    public static float sum(float a, float b) {
+        return a + b;
+    }
+    // prime or not
+    public static boolean isPrime(int n) {
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // primes in range
+    public static void primsInRange(int n) {
+        for(int i = 2; i<=n; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        System.out.println(binoCoeff(5, 2));
+        primsInRange(20);
     }
 }
