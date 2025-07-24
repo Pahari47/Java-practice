fcb25dcf27532f646e62d9c4faf6f44f472c021a
Pahari47 <nachiketapahari00@gmail.com>
2025-07-24 22:45:56 +0530

    Add recursive methods for factorial, natural sum, Fibonacci, sorting check, and first/last occurrence in array

--- 19. Recursion/Recursion.java
+++ 19. Recursion/Recursion.java
@@ -16,8 +16,69 @@ public class Recursion {
         printInc(n-1);
         System.out.print(n+" ");
     }
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * fnm1;
+        return fn;
+    }
+
+    public static int sumNat(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int sumNminusone = sumNat(n-1);
+        int sumN = n + sumNminusone;
+        return sumN;
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fibonacciNM1 = fibonacci(n - 1);
+        int fibonacciNM2 = fibonacci(n - 2);
+        int fibonacciN = fibonacciNM1 + fibonacciNM2;
+        return fibonacciN;
+    }
+
+    public static boolean isSorted(int arr[], int i) {
+        if (i == arr.length-1) {
+            return true;
+        }
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        } else {
+            return isSorted(arr, i+1);
+        }
+    }
+
+    public static int firstOccurence(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        if (arr[i] == key) {
+            return i;
+        }
+        return firstOccurence(arr, key, i+1);
+    }
+
+    public static int lastOccurence(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOccurence(arr, key, i+1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+        return isFound;
+    }
     public static void main(String[] args) {
-        int n = 10;
-        printInc(n);
+        int arr[] = {7, 7, 9, 6, 5, 8, 4, 9, 3, 5};
+
+        System.out.println(lastOccurence(arr, 5, 0));
     }
 }
